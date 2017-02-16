91f9c91bdcf15ff4251c9e151cb751c939f994fe
mpallansch <mkp95@uga.edu>
2017-02-16 14:56:13 -0500

    Major Rewrite

--- js/app.js
+++ js/app.js
@@ -1,19 +1,7 @@
 var motorVehiclesApp = angular.module('motorVehiclesApp', ['motorVehiclesControllers']);
 
-motorVehiclesApp.run(function($rootScope, $http){
-        $rootScope.loading = true;
-        $http.get('test/test.json').then(
-            function(data){
-                $rootScope.loading = false;
-                $rootScope.data = data.data;
-                $rootScope.currentYear = Object.keys($rootScope.data.years)[0];
-                $rootScope.currentState = Object.keys($rootScope.data.years[$rootScope.currentYear].states)[0];
-                $rootScope.currentStrategy = Object.keys($rootScope.data.years[$rootScope.currentYear].states[$rootScope.currentState].implemented)[0];
-            },
-            function(err){
-                console.log(err);
-            }
-        );
+motorVehiclesApp.run(function($rootScope, $http) {
+    
 });
 
 angular.bootstrap(document.body, ['motorVehiclesApp']);
--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,95 @@
 var motorVehiclesControllers = angular.module('motorVehiclesControllers', []);
 
-motorVehiclesControllers.controller('mainCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
-	//$rootScope.variable = 'foo';
+motorVehiclesControllers.controller('mainCtrl', ['$scope', '$http', function($scope, $http){   
+    $scope.loading = true;
+    $scope.pageNumber = 1;
+    $scope.state = 'Alabama';
+    $scope.intervention = 'Alcohol Interlocks';
+    $scope.strategies = ['Alcohol Interlocks','Bicycle Helment','In Person Renewal', 'Increased Seat Belt Fine', 'License Plate Impound', 'Limits on Diversion', 'Motorcycle Helmet', 'Primary Enforcement Seat Belt Law', 'Red Light Camera', 'Saturation Patrols', 'Seat Belt Enforcement Campaign', 'Sobriety Checkpoints', 'Speed Camera', 'Vehicle Impoundment'];
+    
+    angular.element(window).bind('resize', function(){
+        
+    });
+
+    $http.get('test/Data_Infographic_Image1.csv').then(
+            function(response) {
+                $scope.loading = false;
+                
+                var lines = response.data.split('\n');
+                var props = lines[0].split(',');
+
+                for(var i = 0; i < props.length; i++){
+                    if(props[i]){
+                        props[i] = props[i].trim();
+                    }
+                }
+
+                var values, obj;
+                $scope.data = {};
+                for(var i = 1; i < lines.length; i++){
+                    obj = {unimplemented: [], totalInjuries: 0, totalDeaths: 0, totalBenefit: 0, totalCost: 0, totalCostFines: 0};
+                    values = lines[i].split(',');
+
+                    for(var j = 0; j < values.length; j++){
+                        if(props[j]){
+                            obj[props[j]] = values[j];
+                        }
+                    }
+
+                    for(var j = 0; j < $scope.strategies.length; j++){
+                        if(obj[$scope.strategies[j] + ': Intervention Implemented?'] === 'No'){
+                            obj.unimplemented.push($scope.strategies[j]);
+                            obj.totalInjuries += obj[$scope.strategies[j] + ': # of Injuries Reduced'] ? parseInt(obj[$scope.strategies[j] + ': # of Injuries Reduced']) : 0;
+                            obj.totalDeaths += parseInt(obj[$scope.strategies[j] + ': # of Fatalities Reduced']);
+                            obj.totalBenefit += parseInt(obj[$scope.strategies[j] + ': Benefit $/year']);
+                            obj.totalCost += parseInt(obj[$scope.strategies[j] + ' (Fines Excluded): Cost $/year']);
+                            obj.totalCostFines += parseInt(obj[$scope.strategies[j] + ': Cost $/year']);
+                        }
+                    }
+
+                    if(obj['State']){
+                        $scope.data[obj['State'].trim()] = obj;
+                    }
+                }
+                
+
+                $scope.drawMap();
+            },
+            function(err) {
+                console.log(err);
+            }
+    );
+
+    $scope.changePage = function(e){
+        if($(e.target).hasClass('left-arrow') || e.type === 'swiperight'){
+            if($scope.pageNumber > 1){
+                $scope.pageNumber--;
+            }
+        } else if($(e.target).hasClass('right-arrow') || e.type === 'swipeleft'){
+            if($scope.pageNumber < 4){
+                $scope.pageNumber++;
+            }
+        }
+        if(e.type === 'swiperight' || e.type === 'swipeleft'){
+            $scope.$digest();
+        }
+    };
+
+    $scope.drawMap = function(){
+        $scope.map = new Datamap({
+            scope: 'usa',
+            element: document.getElementById('map')
+        });
+    }
+
+}]);
+
+motorVehiclesControllers.directive('swipePage', [function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attr){
+            $(element).on('swipeleft', scope.changePage);
+            $(element).on('swiperight', scope.changePage);
+        }
+    };
 }]);
