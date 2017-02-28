0f559a36b03604574a4427ad6fcbd40efcf04cb1
mpallansch <mkp95@uga.edu>
2017-02-28 11:02:03 -0500

    Automatically Selects Embed Text

--- js/directives.js
+++ js/directives.js
@@ -27,3 +27,16 @@ motorVehiclesDirectives.directive('swipePage', [function() {
             }
         };
     }]);
+
+motorVehiclesDirectives.directive('selectOnClick', ['$window', function($window) {
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+                element.on('click', function() {
+                    if (!$window.getSelection().toString()) {
+                        this.setSelectionRange(0, this.value.length);
+                    }
+                });
+            }
+        };
+    }]);
